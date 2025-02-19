f882e2a32cd7f397b6f921890eb4b1d05868f666
Fajar Kurniawan <19623181@std.stei.itb.ac.id>
2025-02-20 00:40:22 +0700

    refactor: change Block class implementation

--- src/Block.java
+++ src/Block.java
@@ -1,20 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Block {
 
     char[][] block;
+    char symbol;
 
-    public void Str2Block(String blockStr) {
+    List<char[][]> variasi;
+    int varIndex;
+
+    public Block(String blockStr) {
         String[] lines = blockStr.split("\n");
         // split the strings by newline to list of string
         block = new char[lines.length][lines[0].length()];
         // create 2D array of char with the same size as String[] lines
+        for (String line : lines) {
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) != ' ') {
+                    symbol = line.charAt(j);
+                    // set symbol of block
+                    break;
+                }
+            }
+        }
         for (int i = 0; i < lines.length; i++) {
             block[i] = lines[i].toCharArray();
             // convert each string of lines to array of char and put it to block[i]
         }
+
+        variasi = new ArrayList<>();
+        makeVariasi();
+        varIndex = 0;
+    }
+
+    public void makeVariasi() {
+        variasi.add(block);
+        char[][] newBlock = block;
+        for (int i = 0; i < 3; i++) {
+            newBlock = rotate(newBlock);
+            variasi.add(newBlock);
+        }
+        newBlock = mirror(block);
+        variasi.add(newBlock);
+        for (int i = 0; i < 3; i++) {
+            newBlock = rotate(newBlock);
+            variasi.add(newBlock);
+        }
+    }
+
+    public char[][] currentVar() {
+        return variasi.get(varIndex);
+    }
+
+    public void nextVar() {
+        varIndex = (varIndex + 1) % variasi.size();
     }
 
     public void printBlock() {
-        for (char[] chars : block) {
+        for (char[] chars : this.currentVar()) {
             for (char bChar : chars) {
                 System.out.print(bChar);
             }
@@ -22,7 +65,7 @@ public class Block {
         }
     }
 
-    public void rotate() {
+    public char[][] rotate(char[][] block) {
         char[][] newBlock = new char[block[0].length][block.length];
         // panjangnya sama lebarnya ditukar
         for (int i = 0; i < block.length; i++) {
@@ -31,10 +74,10 @@ public class Block {
                 // rotate 90 degree clockwise
             }
         }
-        block = newBlock;
+        return newBlock;
     }
 
-    public void mirror() {
+    public char[][] mirror(char[][] block) {
         char[][] newBlock = new char[block.length][block[0].length];
         // panjang dan lebar tetap
         for (int i = 0; i < block.length; i++) {
@@ -43,6 +86,6 @@ public class Block {
                 // flip horizontally
             }
         }
-        block = newBlock;
+        return newBlock;
     }
 }
